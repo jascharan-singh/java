1e61255566960438f35555c980bfa032694f8bca
singh <singh@DESKTOP-VTL0Q57>
2021-05-14 19:31:03 -0400

    added java-mid-term

---
+++ java/src/java/SyntaxError.java
@@ -0,0 +1,16 @@
+package java;
+import java.util.Scanner;
+
+public class SyntaxError {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Two number to multiply");
+		Scanner keyboard=new Scanner(System.in);
+		int n1=keyboard.nextInt();
+		int n2=keyboard.nextInt();
+		int product= n1*n2;
+		System.out.println("The products is "+ product);
+
+	}
+
+}
